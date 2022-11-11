4259cb773352d828ecba13f5a0113db6e7f2d18d
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-11-11 13:46:21 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -72,11 +72,11 @@ async def gen_thumb(videoid):
         background = image2.filter(filter=ImageFilter.BoxBlur(35))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.8)
-        Xcenter = youtube.width / 2
+        Xcenter = youtube.width / 3
         Ycenter = youtube.height / 2
-        x1 = Xcenter - 300
+        x1 = Xcenter - 250
         y1 = Ycenter - 250
-        x2 = Xcenter + 300
+        x2 = Xcenter + 250
         y2 = Ycenter + 250
         logo = youtube.crop((x1, y1, x2, y2))
         logo.thumbnail((600, 600), Image.ANTIALIAS)
