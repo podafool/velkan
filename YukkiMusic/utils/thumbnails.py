58427f6266b28bb20d7ac90773e6c3863a85b236
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2022-11-30 09:43:26 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -26,7 +26,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
     newImage = image.resize((newHeight, newWidth))
-    newImg = ImageOps.expand(newImage, border=10, fill="yellow")
+    newImg = ImageOps.expand(newImage, border=10, fill="green")
     return newImage
 
 
