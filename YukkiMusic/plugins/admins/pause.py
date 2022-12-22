7f6c50ed1d21ef79fc969e48c59742982ad32350
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2022-12-22 10:06:25 +0530

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -10,7 +10,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from config import BANNED_USERS
+from config import BANNED_USERS, PAUSE_IMG_URL
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
@@ -35,6 +35,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
     await Yukki.pause_stream(chat_id)
-    await message.reply_text(
-        _["admin_2"].format(message.from_user.mention)
+    await message.reply_photo(
+        photo=PAUSE_IMG_URL
+        caption=_["admin_2"].format(message.from_user.mention)
     )
