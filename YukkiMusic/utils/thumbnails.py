7928cd425aab86f64f71bdec34fbc307ed9ca8e6
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-20 22:11:51 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -136,7 +136,7 @@ async def gen_thumb(videoid, user_id):
             font=font2,
         )
         draw.text(
-            (800, 450),
+            (800, 480),
             "the",
             fill="white",
             stroke_width=2,
@@ -144,7 +144,7 @@ async def gen_thumb(videoid, user_id):
             font=font2,
         )
         draw.text(
-            (880, 550),
+            (900, 550),
             "song",
             fill="white",
             stroke_width=2,
