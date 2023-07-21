45c86052147d16421a13684b65b6934f96cd5fa1
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2023-07-21 18:52:48 +0530

    Update newgroup.py

--- YukkiMusic/plugins/bot/newgroup.py
+++ YukkiMusic/plugins/bot/newgroup.py
@@ -11,8 +11,13 @@ async def new_message(chat_id: int, message: str):
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.mention if message.from_user else "Unknown User"
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "Private Group"
+        user = message.from_user.mention
+        username = message.from_user.username
         title = message.chat.title
         chat_id = message.chat.id
-        Insane = f"✨ #New_Group 😍\n\n🧡✨ Chat ID : {chat_id}\n✨💛 Chat Title : {title}\n💚✨ Username : {added_by}\n✨💙 User ID : {message.from_user.id}"
+        Insane = f"✨ **#New_Group** 😍\n\n🧡✨ **Chat ID :** {chat_id}\n✨💛 **Chat Title :** {title}\n💚✨ **User :** {user}\n✨💗 **Username :** @{username}\n💙✨ **User ID :** {message.from_user.id}\n✨💜 **Chat link :** {chatusername}"
         await new_message(LOG_GROUP_ID, Insane)
