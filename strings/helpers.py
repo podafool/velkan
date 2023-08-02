35511f332eb2fd3e8132f17682066c2039257aa2
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-02 16:44:23 +0530

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -37,7 +37,7 @@ HELP_2 = """🗯 For channels
 /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
 
 /cplay  - Bot will start playing your given query on Stream live links on voice chats.
-/cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+/cplayforce -  Similar to /playforce but use for channel
 /cpause - Pause the playing music.
 
 /cresume- Resume the paused music.
@@ -51,13 +51,11 @@ HELP_2 = """🗯 For channels
 
 /cqueue- Check Queue List of Music.
 
-/cskip [Number(example: 3)] 
-    - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
- /cloop [enable/disable] or [Numbers between 1-10] 
-    - When activated, bot loops the current playing music to 1-10 times on voice chat. Default to 10 times."""
+/cskip [Number(example: 3)] - Skips music to a the specified queued number.
+ /cloop [enable/disable] or [Numbers between 1-10] - Bot loops the current playing music to 1-10 times on voice chat. Default to 10 times."""
 
 
-HELP_3 = """🦚 Other commands
+HELP_3 = """🎛 Other commands
 
 /start - Start the Music Bot.
 /help  - Get Commands Helper Menu with detailed explanations of commands.
@@ -81,28 +79,17 @@ HELP_4 = """⚙️ Settings commands
 
 /settings - Get a complete group's settings with inline buttons
 
-  Options in Settings:
-
+  **Options in Settings:**
 1️. Set Audio Quality.
-
 2️. Set Video Quality.
-
 3️. Auth Users:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
 4️. Clean Mode: When enabled deletes the bot's messages after 5 mins from your group.
-
 5. Command Clean : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
-
 6️. Play Settings:
 
 /playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
 
 Options in playmode:
-
 1️. Search Mode [Direct or Inline] - Changes your search mode while you give /play mode. 
-
 2️. Admin Commands [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
 3️. Play Type [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
-
-HELP_5 = """None"""
