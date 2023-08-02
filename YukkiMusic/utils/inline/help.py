9b4e9a42c1b4786c0ae792fb84dcaefad4f12db4
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-02 15:32:21 +0530

    buttons

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -32,7 +32,7 @@ def help_pannel(_, START: Union[bool, int] = None):
     mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
-            [
+            
                 InlineKeyboardButton(
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
@@ -41,8 +41,8 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
-            ],
-            [
+            
+            
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
@@ -50,14 +50,8 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb5",
-                ),
-            ],
+                ),            
+            
             mark,
         ]
     )
