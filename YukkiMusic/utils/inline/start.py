f5f33544f8377fa991306983d281cc5cde4e285d
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-02 18:38:34 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -60,7 +60,7 @@ def start_pannel(_):
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
-        if OWNER:
+    if OWNER:
             buttons.append(
                 [
                     InlineKeyboardButton(
