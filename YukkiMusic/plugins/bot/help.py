0d632f3fcaa7c6113a49d6a9f04ce50092f0f3e0
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-02 16:24:48 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -52,8 +52,10 @@ async def helper_private(
         keyboard = help_pannel(_, True)
         if update.message.photo:
             await update.message.delete()
-            await update.message.reply_text(
-                _["help_1"], reply_markup=keyboard
+            await update.message.reply_photo(
+                photo="https://telegra.ph/file/803ffb6adaeb83215a8d0.jpg",
+                caption=_["help_1"],
+                reply_markup=keyboard
             )
         else:
             await update.edit_message_text(
