065b3b5aa7d509774043f9aa53152235113177b6
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-03 07:12:21 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -58,7 +58,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} has just started bot to check <code>HELP INFORMATION</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
+                    f"{message.from_user.mention} has just started bot to check <code>HELP COMMANDS</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
                 )
                         
             return  
