533c67cbf7c7f73435d21c1a14d53ccaf69e9f8d
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2023-08-03 14:52:01 +0530

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -187,9 +187,9 @@ async def stream(
                 caption=_["queue_4"].format(
                     position, title[:30], duration_min, user_name
                 ),
-            await asyncio.sleep(1),
-            await app.send_sticker("CAACAgUAAxkBAAEJ511ky3CVJRVZGvGXdQZ1pNJLbrE9VQACdAYAAiFP4VQEeuQWBclToC8E"),
             )
+            await asyncio.sleep(1)
+            await app.send_sticker("CAACAgUAAxkBAAEJ511ky3CVJRVZGvGXdQZ1pNJLbrE9VQACdAYAAiFP4VQEeuQWBclToC8E")           
         else:
             if not forceplay:
                 db[chat_id] = []
