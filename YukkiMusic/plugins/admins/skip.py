22b884de57ce39b0cb70fa866b87d8450f8749ef
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-05 12:19:55 +0530

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -10,6 +10,8 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
+import random
+import asyncio
 import config
 from config import BANNED_USERS
 from strings import get_command
@@ -22,6 +24,33 @@ from YukkiMusic.utils.inline.play import (stream_markup,
                                           telegram_markup)
 from YukkiMusic.utils.stream.autoclear import auto_clean
 from YukkiMusic.utils.thumbnails import gen_thumb
+
+STICKERS = [
+  "CAACAgQAAxkBAAEJ7AhkzQ7GZ7DrL3O4Q7eHVCAYz-N4nwACvQkAAnpcEVM6alQk5njq3y8E",
+  "CAACAgQAAxkBAAEJ7ARkzQ60YZZ7t4ivO7K8VR0LQifh9gACFQwAAtUjEFPkKwhxHG8_Ky8E",
+  "CAACAgQAAxkBAAEJ7B1kzRHZ8-XDcyZNUE7Qyc7lsdwFMQACjggAA1VQUdoUwOeQzZqmLwQ",
+  "CAACAgQAAxkBAAEJ7AJkzQ6yOkDOwj9r01b7fljN_Boh9wAC6gsAAmwiEVOtWUCotxfPAy8E",
+  "CAACAgQAAxkBAAEJ7AABZM0OsGD_J8puJTi9WkLqWQG-SAADuBEAAqbxcR57Dj3-S9mwaS8E",
+  "CAACAgQAAxkBAAEJ6_5kzQ6u16es2S8IVUSSQrA9hi_vkwACnxEAAqbxcR57wYUDyflSIS8E",
+  "CAACAgEAAxkBAAEJ6_xkzQ50xN3ytZjk5fTylx7DS2PDVgACNgEAAlEpDTkSG_gDZwABw6MvBA",
+  "CAACAgEAAxkBAAEJ6_pkzQ5lhNGO3pF1awcVfWxfBC_lQwACGQEAAlEpDTkG9n5mFbHKpy8E",
+  "CAACAgEAAxkBAAEJ6_hkzQ5jChDHyCPnrf-xuCQQtouztAACFQEAAlEpDTnRN1QlsQ8qLi8E",
+  "CAACAgUAAxkBAAEJ6_ZkzQ3ldTCPnhslPTxQUoirypK47wACowYAAkME2FZILCjifFdIUC8E",
+  "CAACAgQAAxkBAAEJ6_BkzQ2qVDgusETUthPZSJ0l4YyKyAACLwoAAgM8IFMao7hilxhkGi8E",
+  "CAACAgEAAxkBAAEJ6-5kzQ2UPyvOoBhBh55zDwnpxy2S2QAC8wQAAlEpDTmH9fRvHZACii8E",
+  "CAACAgQAAxkBAAEJ6-xkzQ1cU-Oxv0vMWC1Hy-uhlASEAwACpwoAAn-aOFAK54ox7NBRcC8E",
+  "CAACAgQAAxkBAAEJ6-pkzQ0mL58wlqP6tTloYWOxYbwFgQACXgwAAghGuVOWomIaBycL7i8E",
+  "CAACAgIAAxkBAAEJ66dkzPAnyyPwli7yRX1hpMMQb7PJTgACDQEAAladvQpG_UMdBUTXly8E",
+  "CAACAgEAAxkBAAEJ66FkzPARmsJrT_FfgYn1A7BumF3CnwACuwADUSkNOR12rpeAPL_kLwQ",
+  "CAACAgEAAxkBAAEJ655kzPANksJJTQXkWl1q1E729tegAgACuAADUSkNOeiAtZ8X-LsKLwQ",
+  "CAACAgEAAxkBAAEJ65tkzPAH_xsIpOKY3y6pugABWnGYHdsAArMAA1EpDTkH2Th_5u9jEy8E",
+  "CAACAgEAAxkBAAEJ65lkzO_5oUKK3Z5k8JTOjLW62Vr9gwACmgADUSkNOfUGBWVzkcCyLwQ",
+  "CAACAgEAAxkBAAEJ65VkzO_yfQABWNWzp75LTIFwfN4PhFsAApMAA1EpDTkdCAmv9TYB9i8E",
+  "CAACAgEAAxkBAAEJ65NkzO_qf0xu4BaRSEAEfKmVmYo9EAACjAADUSkNOaEz-mHfkE3aLwQ",
+  "CAACAgQAAxkBAAEJ64ZkzO7Je62eg3T6QZNxgvNXMxQYzAACpRYAAqbxcR7qDYebQsdZoi8E",
+  "CAACAgQAAxkBAAEJ6pVkzHkvmfmavL8AAZO8Chh1WOJn7WYAAmcNAAKYxTlQbCCYZOix0kQvBA",
+  "CAACAgQAAxkBAAEJ6ptkzHoGfPr3wnuhsSowP3fin1iWhgACjggAAovV6FOaebAKJCO-5C8E",
+]
 
 # Commands
 SKIP_COMMAND = get_command("SKIP_COMMAND")
@@ -172,7 +201,7 @@ async def skip(cli, message: Message, _, chat_id):
         )
         db[chat_id][0]["mystic"] = run
         db[chat_id][0]["markup"] = "stream"
-        await message.reply_sticker("CAACAgQAAxkBAAEJ6pVkzHkvmfmavL8AAZO8Chh1WOJn7WYAAmcNAAKYxTlQbCCYZOix0kQvBA") # heart vechitu jumping : queue la video song ku skip pannum bothu
+        await message.reply_sticker(random.choice(STICKERS)) #Munnadi heart vechitu jumping : queue la video song ku skip pannum bothu
         await mystic.delete()
         
     elif "index_" in queued:
@@ -181,7 +210,7 @@ async def skip(cli, message: Message, _, chat_id):
         except Exception:
             return await message.reply_text(_["call_9"])
         button = telegram_markup(_, chat_id)
-        await message.reply_sticker("CAACAgQAAxkBAAEJ6plkzHmxI62tjrxsXGCfFY7YmFGiLAACPgsAAsCwiFHs4-FX1TKh4S8E") #blowing heart
+        await message.reply_sticker("CAACAgQAAxkBAAEJ6plkzHmxI62tjrxsXGCfFY7YmFGiLAACPgsAAsCwiFHs4-FX1TKh4S8E") #blowing heart : for m3u8 links (live streaming links)
         run = await message.reply_photo(
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
@@ -208,7 +237,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await asyncio.sleep(1)
-            await message.reply_sticker("CAACAgQAAxkBAAEJ6ptkzHoGfPr3wnuhsSowP3fin1iWhgACjggAAovV6FOaebAKJCO-5C8E") #giving big heart : queue la telegram audio file tag panni /play pannirundha
+            await message.reply_sticker(random.choice(STICKERS)) #Munnadi giving big heart : queue la telegram audio file tag panni /play pannirundha
         elif videoid == "soundcloud":
             button = telegram_markup(_, chat_id)
             run = await message.reply_photo(
@@ -224,8 +253,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid, chat_id)
-            await message.reply_sticker("CAACAgIAAxkBAAEJ6p1kzHpCYgPd3ywh6YieMvwZB_oCfgACMAEAAsxUSQkLdSnCzPHMMS8E") #cute bunny girl : normal ah /play pannirundha
+            img = await gen_thumb(videoid, chat_id)            
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -236,3 +264,5 @@ async def skip(cli, message: Message, _, chat_id):
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
+            await asyncio.sleep(1)
+            await message.reply_sticker(random.choice(STICKERS)) #Munnadi cute bunny girl : normal ah /play pannirundha
