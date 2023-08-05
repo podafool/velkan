a2ca6f238bd376368004ad0b60e0660d37bc242b
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-05 20:12:29 +0530

    Update queue.py

--- YukkiMusic/utils/inline/queue.py
+++ YukkiMusic/utils/inline/queue.py
@@ -6,7 +6,7 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-
+import math
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
