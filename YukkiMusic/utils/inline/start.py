e09649149e29b8eb3dca0b5ce5f002e97bb413d0
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-05 19:06:54 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -82,6 +82,9 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
                 ),
                 InlineKeyboardButton(
+                text=_["S_B_8"], callback_data="settings_back_helper"
+                ), 
+                InlineKeyboardButton(
                     text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
                 ),
             ]
