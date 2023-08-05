acfcf8df0160e957824fb4d50f756b82f590bc1f
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-05 23:45:34 +0530

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -6,7 +6,7 @@
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
-
+import asyncio
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -32,9 +32,10 @@ async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     await Yukki.stop_stream(chat_id)
-    await set_loop(chat_id, 0)
-    await message.reply_sticker("CAACAgQAAxkBAAEJ6pFkzHeFqLzq8avJ9DoXgUeICSX1PgACuwkAAuQGCVLowXK3fUF5SC8E")
+    await set_loop(chat_id, 0)    
     await message.reply_photo(
         photo="https://te.legra.ph/file/218a2e7560a152c03db69.jpg",
         caption=_["admin_9"].format(message.from_user.mention)
     )
+    await asyncio.sleep(1)
+    await message.reply_sticker("CAACAgQAAxkBAAEJ6pFkzHeFqLzq8avJ9DoXgUeICSX1PgACuwkAAuQGCVLowXK3fUF5SC8E")
