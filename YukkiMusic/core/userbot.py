8e6809e54ac0226e35655475d1d1e3dcba60ca86
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-16 15:16:27 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -22,35 +22,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name=client_1,
+            name="client_1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string =str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name=client_2,
+            name="client_2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string =str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name=client_3,
+            name="client_3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string =str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name=client_4,
+            name="client_4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string =str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name=client_5,
+            name="client_5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
