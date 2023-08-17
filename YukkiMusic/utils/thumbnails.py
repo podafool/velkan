05abba84e75b0057e4be0b06a56010e77df48b07
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-17 07:50:07 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -64,10 +64,11 @@ async def gen_thumb(videoid, user_id):
                         duration = result["duration"]
                     except:
                         duration = "Unknown"
-                    thumbnail = result["thumbnails"][0]["url"].split("?")[0]                    
+                    #thumbnail = result["thumbnails"][0]["url"].split("?")[0]                    
                 
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
                 if resp.status == 200:
                     f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
                     await f.write(await resp.read())
