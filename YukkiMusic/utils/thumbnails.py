36e94eb4cac129b1827e8afc3cc72c447f47ad36
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-18 10:01:22 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -39,7 +39,7 @@ async def gen_thumb(videoid, user_id):
         return f"cache/{videoid}_{user_id}.png"
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
-        results = VideosSearch(url, limit=1)
+        results = await VideosSearch(url)
         for result in (await results.next())["result"]:
             try:
                 title = result["title"]
