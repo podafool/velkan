d77c89df0fbef29d58528221ececb4300cf504f8
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-18 16:35:02 +0530

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -46,6 +46,9 @@ async def pause_admin(cli, message: Message, _, chat_id):
     )
     await asyncio.sleep(1)
     await message.reply_sticker("CAACAgEAAxkBAAEJ6nxkzFywBJ_zMhOj0wc7AQgV9cLNXwAC3QQAAlEpDTmUHImRSVIUeS8E")
-    await client.reply_sticker("CAACAgEAAxkBAAEJ6nxkzFywBJ_zMhOj0wc7AQgV9cLNXwAC3QQAAlEpDTmUHImRSVIUeS8E")
+    #await client.reply_sticker("CAACAgEAAxkBAAEJ6nxkzFywBJ_zMhOj0wc7AQgV9cLNXwAC3QQAAlEpDTmUHImRSVIUeS8E")
+    for num in assistants:
+            client = await get_client(num)
+            await client.reply_sticker("CAACAgEAAxkBAAEJ6nxkzFywBJ_zMhOj0wc7AQgV9cLNXwAC3QQAAlEpDTmUHImRSVIUeS8E")
     
     
