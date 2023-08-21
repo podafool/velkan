dd96b33980b7a92eb8f4cd869dd03fc2a3544407
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-21 22:12:02 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -126,7 +126,7 @@ async def gen_thumb(videoid, user_id):
             x = f.resize((245, 245))
     
             yield x
-        async for x in async_process_user_profile(app, user_id):
+        async for x in process_user_profile(app, user_id):
             background.paste(x, (947, 118), mask=x)
         print("Success!")
 
