82a0485cbb86b10ea67d2fc778cefeddab76a018
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-21 19:43:33 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -29,7 +29,7 @@ def clear(text):
     return title.strip()
 
 
-async def gen_thumb(videoid):
+async def gen_thumb(videoid, user_id):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
 
