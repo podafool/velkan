287beff04c6d0c4bacc13265084db5dbf947ed2e
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-21 20:29:33 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -35,10 +35,10 @@ def add_corners(im):
 
 async def process_user_profile(app, user_id):
     try:
-        wxyz = await app.get_profile_photos(user_id)
+        wxyz = await app.get_chat_photos(user_id)
         wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
     except:
-        hehe = await app.get_profile_photos(app.id)
+        hehe = await app.get_chat_photos(app.id)
         wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
 
     xy = Image.open(wxy)
