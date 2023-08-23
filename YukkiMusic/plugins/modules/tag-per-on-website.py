2c5f8102d0e55b903fd784685a9d0cbc6331e4fa
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-08-23 09:22:54 +0530

    Update tag-per-on-website.py

--- YukkiMusic/plugins/modules/tag-per-on-website.py
+++ YukkiMusic/plugins/modules/tag-per-on-website.py
@@ -107,7 +107,7 @@ async def tagu_handler(client, message: Message):
     usrnum = 0
     usrtxt = ""
 
-    async for usr in client.iter_chat_members(chat_id):
+    async for usr in client.get_chat_members(chat_id):
         if not chat_id in spam_chats:
             break
 
