4ae5beaaef99f17353d6b3806f05bf951acb38d9
Velakenna <141400352+Velakenna@users.noreply.github.com>
2023-11-19 03:03:06 +0530

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -85,6 +85,18 @@ class YouTubeAPI:
             return None
         return text[offset : offset + length]
 
+    async def title(
+        self, link: str, videoid: Union[bool, str] = None
+    ):
+        if videoid:
+            link = self.base + link
+        if "&" in link:
+            link = link.split("&")[0]
+        results = VideosSearch(link, limit=1)
+        for result in (await results.next())["result"]:
+            title = result["title"]
+        return title
+
     async def details(
         self, link: str, videoid: Union[bool, str] = None
     ):
@@ -102,19 +114,7 @@ class YouTubeAPI:
                 duration_sec = 0
             else:
                 duration_sec = int(time_to_seconds(duration_min))
-        return title, duration_min, duration_sec, thumbnail, vidid
-
-    async def title(
-        self, link: str, videoid: Union[bool, str] = None
-    ):
-        if videoid:
-            link = self.base + link
-        if "&" in link:
-            link = link.split("&")[0]
-        results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-        return title
+        return title, duration_min, duration_sec, thumbnail, vidid    
 
     async def duration(
         self, link: str, videoid: Union[bool, str] = None
